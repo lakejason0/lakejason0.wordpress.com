23f09d856815cc12cdc1ff842ad579c912a0ef48
lakejason0 <36039861+lakejason0@users.noreply.github.com>
2018-07-06 09:59:31 +0800

    Update script

---
+++ Lake桑的存档馆.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Lake桑的存档馆
+// @namespace    https://lakejason0.wordpress.com/
+// @include      https://lakejason0.wordpress.com/*
+// @include      http://lakejason0.wordpress.com/*
+// @version      0.1
+// @description  Lake桑的存档馆。增强Crisp。
+// @author       Lake桑
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+
+window.$crisp=[];
+window.CRISP_WEBSITE_ID="0e35ddce-1fe7-4f98-b916-3a83ed049d82";
+(function(){
+    d=document;
+    s=d.createElement("script");
+    s.src="https://client.crisp.chat/l.js";
+    s.async=1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
